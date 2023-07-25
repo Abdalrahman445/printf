8ec21298fd5eb6637dcca24225a132c3fbdd3711
Mahmoud Alazhary <mahmoudalazhary2@gmail.com>
2023-07-25 18:46:03 +0300

    Updated

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#define BUFFSIZE 1024
+
+int my_putchar(char c);
+int my_putstr(char* s);
+int _printf(const char* format, ...);
+
+#endif
+
